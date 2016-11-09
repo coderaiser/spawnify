6aa5205be1bbe756eb2e6a4853c325d4aa5a7e07
coderaiser <coderaiser@cloudcmd.io>
2016-11-09 13:36:14 +0200

    chore(spawnify) rm iife

--- bin/spawnify.js
+++ bin/spawnify.js
@@ -1,28 +1,27 @@
 #!/usr/bin/env node
 
-(function() {
-    'use strict';
+'use strict';
+
+var spawn,
+    spawnify    = require('../'),
+    argv        = process.argv.slice(2),
+    command     = argv.join(' ');
+
+if (!command) {
+    console.log('spawnify <command>');
+} else {
+    spawn = spawnify(command);
+    spawn.on('data', function(data) {
+        console.log(data);
+    });
     
-    var spawn,
-        spawnify    = require('../'),
-        argv        = process.argv.slice(2),
-        command     = argv.join(' ');
+    spawn.on('error', function(error) {
+        console.error(error.message);
+    });
     
-    if (!command) {
-        console.log('spawnify <command>');
-    } else {
-        spawn = spawnify(command);
-        spawn.on('data', function(data) {
-            console.log(data);
-        });
-        
-        spawn.on('error', function(error) {
-            console.error(error.message);
-        });
-        
-        spawn.on('close', function() {
-            console.log('closed');
-            spawn = null;
-        });
-    }
-})();
+    spawn.on('close', function() {
+        console.log('closed');
+        spawn = null;
+    });
+}
+
