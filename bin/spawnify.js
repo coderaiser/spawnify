3430779ac687d80efa4d525aef9999edb3a1d99e
coderaiser <coderaiser@cloudcmd.io>
2021-01-20 19:23:04 +0200

    feature(spawnify) drop support of node < 14

--- bin/spawnify.js
+++ bin/spawnify.js
@@ -2,24 +2,24 @@
 
 'use strict';
 
-var spawn,
-    spawnify    = require('../'),
-    argv        = process.argv.slice(2),
-    command     = argv.join(' ');
+let spawn;
+const spawnify = require('../');
+const argv = process.argv.slice(2);
+const command = argv.join(' ');
 
 if (!command) {
     console.log('spawnify <command>');
 } else {
     spawn = spawnify(command);
-    spawn.on('data', function(data) {
+    spawn.on('data', (data) => {
         console.log(data);
     });
     
-    spawn.on('error', function(error) {
+    spawn.on('error', (error) => {
         console.error(error.message);
     });
     
-    spawn.on('close', function() {
+    spawn.on('close', () => {
         console.log('closed');
         spawn = null;
     });
